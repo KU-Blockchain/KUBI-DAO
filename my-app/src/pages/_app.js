3b628fc67f8b1e47c713b2e3ac0414d6e411e2a0
hudsonhrh <hudsonhrh7@gmail.com>
2023-03-29 17:46:39 -0500

    changed config for paths

--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import Navbar from '../components/Navbar';
+import Navbar from 'src/components/Navbar';
 
 const theme = extendTheme({
   config: {
