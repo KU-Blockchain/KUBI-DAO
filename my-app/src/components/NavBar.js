1d3827cd57a4e0c202654b274fc7ce210c8bebed
hudsonhrh <hudsonhrh7@gmail.com>
2023-03-29 17:32:01 -0500

    fixed navbar hydration error

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Box, Flex, Button } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 const Navbar = () => {
   return (
     <Box bg="blue.500" p={4}>
       <Flex justifyContent="space-around">
-        <NextLink href="/" passHref>
-          <Button as="a" color="white" fontWeight="bold">Home</Button>
-        </NextLink>
-        <NextLink href="/tasks" passHref>
-          <Button as="a" color="white" fontWeight="bold">Tasks</Button>
-        </NextLink>
-        <NextLink href="/leaderboard" passHref>
-          <Button as="a" color="white" fontWeight="bold">Leaderboard</Button>
-        </NextLink>
-        <NextLink href="/voting" passHref>
-          <Button as="a" color="white" fontWeight="bold">Voting</Button>
-        </NextLink>
-        <NextLink href="/user" passHref>
-          <Button as="a" color="white" fontWeight="bold">User</Button>
-        </NextLink>
+        <Link as={NextLink} href="/" color="white" fontWeight="bold">
+          Home
+        </Link>
+        <Link as={NextLink} href="/tasks" color="white" fontWeight="bold">
+          Tasks
+        </Link>
+        <Link as={NextLink} href="/leaderboard" color="white" fontWeight="bold">
+          Leaderboard
+        </Link>
+        <Link as={NextLink} href="/voting" color="white" fontWeight="bold">
+          Voting
+        </Link>
+        <Link as={NextLink} href="/user" color="white" fontWeight="bold">
+          User
+        </Link>
       </Flex>
     </Box>
   );
 };
 
 export default Navbar;
-
