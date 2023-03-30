c063e4724ff710f20118ea8a5f8cad09f179f44e
hudsonhrh <hudsonhrh7@gmail.com>
2023-03-29 20:01:53 -0500

    updated project sidebar to create add project button

--- my-app/src/components/ProjectSidebar.js
+++ my-app/src/components/ProjectSidebar.js
@@ -1,11 +1,16 @@
-// src/components/ProjectSidebar.js
-import React from 'react';
-import { Box, VStack, Heading, Button } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, VStack, Heading, Button, Input, FormControl } from '@chakra-ui/react';
 
-const ProjectSidebar = ({ projects, onSelectProject }) => {
+const ProjectSidebar = ({ projects, onSelectProject, onCreateProject }) => {
+  const [newProjectName, setNewProjectName] = useState('');
+
+  const handleCreateProject = () => {
+    onCreateProject(newProjectName);
+    setNewProjectName('');
+  };
+
   return (
     <Box bg="gray.200" w="fit-content" h="100%" p={4} borderRight="1px" borderColor="gray.300">
-
       <Heading size="md" mb={4}>Projects</Heading>
       <VStack spacing={4} align="start">
         {projects.map((project) => (
@@ -13,6 +18,16 @@ const ProjectSidebar = ({ projects, onSelectProject }) => {
             {project.name}
           </Button>
         ))}
+        <FormControl>
+          <Input
+            placeholder="New project name"
+            value={newProjectName}
+            onChange={(e) => setNewProjectName(e.target.value)}
+          />
+        </FormControl>
+        <Button onClick={handleCreateProject} disabled={!newProjectName.trim()}>
+          Create Project
+        </Button>
       </VStack>
     </Box>
   );
