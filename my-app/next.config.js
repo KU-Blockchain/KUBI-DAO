ecd002b6b03af3da7acf0f2a4df208078029daa6
hudsonhrh <hudsonhrh7@gmail.com>
2023-03-29 19:18:52 -0500

    please work

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,18 +1,19 @@
-const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')([]);
 const path = require('path');
-
 
 const nextConfig = {
   webpack: (config, { isServer }) => {
     // Resolve aliases
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@/components': path.resolve(__dirname, 'components'),
+    config.resolve = {
+      ...config.resolve,
+      baseUrl: path.resolve(__dirname),
+      alias: {
+        ...config.resolve.alias,
+        '@/components': path.resolve(__dirname, 'components'),
+      },
     };
 
     return config;
   },
 };
 
-module.exports = withPlugins([withTM], nextConfig);
+module.exports = nextConfig;
