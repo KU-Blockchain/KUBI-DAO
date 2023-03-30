df74370517a365dbfced96df8913bd2d7824a64e
hudsonhrh <hudsonhrh7@gmail.com>
2023-03-29 20:34:05 -0500

    added basic Homepage

--- my-app/src/components/ProjectSidebar.js
+++ my-app/src/components/ProjectSidebar.js
@@ -1,34 +1,72 @@
 import React, { useState } from 'react';
-import { Box, VStack, Heading, Button, Input, FormControl } from '@chakra-ui/react';
+import {
+  Box,
+  VStack,
+  Heading,
+  Button,
+  Input,
+  FormControl,
+  Spacer,
+  Flex,
+} from '@chakra-ui/react';
 
 const ProjectSidebar = ({ projects, onSelectProject, onCreateProject }) => {
   const [newProjectName, setNewProjectName] = useState('');
+  const [showInput, setShowInput] = useState(false);
 
   const handleCreateProject = () => {
     onCreateProject(newProjectName);
     setNewProjectName('');
+    setShowInput(false);
   };
 
   return (
-    <Box bg="gray.200" w="fit-content" h="100%" p={4} borderRight="1px" borderColor="gray.300">
-      <Heading size="md" mb={4}>Projects</Heading>
-      <VStack spacing={4} align="start">
-        {projects.map((project) => (
-          <Button key={project.id} onClick={() => onSelectProject(project.id)}>
-            {project.name}
-          </Button>
-        ))}
-        <FormControl>
-          <Input
-            placeholder="New project name"
-            value={newProjectName}
-            onChange={(e) => setNewProjectName(e.target.value)}
-          />
-        </FormControl>
-        <Button onClick={handleCreateProject} disabled={!newProjectName.trim()}>
-          Create Project
+    <Box
+      bg="gray.200"
+      w="300px"
+      h="100vh"
+      p={4}
+      borderRight="1px"
+      borderColor="gray.300"
+      display="flex"
+      flexDirection="column"
+    >
+      <Heading size="md" mb={4}>
+        Projects
+      </Heading>
+      <Box flexGrow={1} overflowY="auto" pr={4}>
+        <VStack spacing={4} align="start">
+          {projects.map((project) => (
+            <Button
+              key={project.id}
+              onClick={() => onSelectProject(project.id)}
+              width="100%" // Set width to 100%
+            >
+              {project.name}
+            </Button>
+          ))}
+        </VStack>
+      </Box>
+      <Spacer />
+      <Flex direction="column" mt={4}>
+        {showInput && (
+          <FormControl>
+            <Input
+              placeholder="New project name"
+              value={newProjectName}
+              onChange={(e) => setNewProjectName(e.target.value)}
+            />
+          </FormControl>
+        )}
+        <Button
+          mt={2}
+          onClick={showInput ? handleCreateProject : () => setShowInput(true)}
+          disabled={showInput && !newProjectName.trim()}
+          width="100%" // Set width to 100%
+        >
+          {showInput ? 'Save Project' : 'Create Project'}
         </Button>
-      </VStack>
+      </Flex>
     </Box>
   );
 };
--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,14 +1,49 @@
-// home.js
-
 import React from 'react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  VStack,
+  Container,
+} from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <VStack spacing={10}>
+      <Box bg="blue.500" w="100%" py={20}>
+        <Container centerContent>
+          <Heading as="h1" size="2xl" color="white">
+            Welcome to KUBI DAO
+          </Heading>
+          <Text fontSize="xl" color="white" mt={5}>
+            The Universitty of Kansas Blockchain Institute's decentralized platform for collaboration and reward distribution
+          </Text>
+          <Button colorScheme="whiteAlpha" mt={8}>
+            Get Started
+          </Button>
+        </Container>
+      </Box>
+
+      <Container centerContent>
+        <VStack spacing={5}>
+          <Heading as="h2" size="xl">
+            About KUBI DAO
+          </Heading>
+          <Text fontSize="lg" textAlign="center">
+            KUBIX DAO is a decentralized autonomous organization that offers a
+            unique model for collaboration, incentives, and privileges. Our
+            hybrid tokenomics system combines KUBIX and KUBI tokens to create a
+            platform that encourages active participation and fair
+            distribution of rewards.
+          </Text>
+        </VStack>
+      </Container>
+
+      {/* Add more sections as needed */}
+    </VStack>
   );
 };
 
 export default Home;
-
