25cb6265c2bc2a53405382e26db0febdd4295d15
hudsonhrh <hudsonhrh7@gmail.com>
2023-03-31 09:15:24 -0500

    project buttons close to working

--- my-app/src/components/MainLayout.js
+++ my-app/src/components/MainLayout.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Flex } from '@chakra-ui/react';
 import ProjectSidebar from './ProjectSidebar';
 import TaskBoard from './TaskBoard';
+import { TaskBoardProvider } from '../contexts/TaskBoardContext';
 
 const MainLayout = () => {
   const initialColumns = [
@@ -36,37 +37,67 @@ const MainLayout = () => {
    
     
 }];
-
 const [projects, setProjects] = useState([
-{ id: 1, name: 'Project 1', columns: initialColumns },
-{ id: 2, name: 'Project 2', columns: initialColumns },
-{ id: 3, name: 'Project 3', columns: initialColumns },
+  { id: 1, name: 'Project 1', columns: JSON.parse(JSON.stringify(initialColumns)) },
+  { id: 2, name: 'Project 2', columns: JSON.parse(JSON.stringify(initialColumns)) },
+  { id: 3, name: 'Project 3', columns: JSON.parse(JSON.stringify(initialColumns)) },
 ]);
 
+
+
+
+
 const [selectedProject, setSelectedProject] = useState(projects[0]);
+
+useEffect(() => {
+  handleSelectProject(projects[0].id);
+}, []);
+
+
+const handleUpdateColumns = (newColumns) => {
+  setProjects(
+    projects.map((project) =>
+      project.id === selectedProject.id ? { ...project, columns: newColumns } : project
+    )
+  );
+};
+
 
 const handleSelectProject = (projectId) => {
     const selected = projects.find((project) => project.id === projectId);
     setSelectedProject(selected);
   };
 
-const handleCreateProject = (projectName) => {
+  const handleCreateProject = (projectName) => {
     const newProject = {
       id: projects.length + 1,
       name: projectName,
-      columns: [], // Start with an empty array or any initial columns
+      columns: [
+        { id: "open", title: "Open", tasks: [] },
+        { id: "inProgress", title: "In Progress", tasks: [] },
+        { id: "inReview", title: "In Review", tasks: [] },
+        { id: "completed", title: "Completed", tasks: [] },
+      ],
     };
     setProjects([...projects, newProject]);
   };
+  
 
   return (
     <Flex w="100%" h="100vh">
       <ProjectSidebar
         projects={projects}
+        selectedProject={selectedProject}
         onSelectProject={handleSelectProject}
         onCreateProject={handleCreateProject}
       />
-      <TaskBoard columns={selectedProject.columns} />
+      <TaskBoardProvider
+        projectId={selectedProject.id}
+        initialColumns={selectedProject.columns}
+        onColumnChange={(newColumns) => handleUpdateColumns(newColumns)}
+      >
+        <TaskBoard />
+      </TaskBoardProvider>
     </Flex>
   );
 };
--- my-app/src/components/TaskBoard.js
+++ my-app/src/components/TaskBoard.js
@@ -2,42 +2,40 @@ import React from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { TaskBoardProvider} from '../contexts/TaskBoardContext';
+import { TaskBoardProvider, useTaskBoard } from '../contexts/TaskBoardContext';
 import TaskColumn from './TaskColumn';
 
-const TaskBoard = ({ columns }) => {
-  
-  return (
-    <TaskBoardProvider initialColumns={columns}>
-      <DndProvider backend={HTML5Backend}>
-        <Flex
-          direction={{ base: 'column', md: 'row' }}
-          justifyContent='space-between'
-          w='100%'
-          h='calc(100vh - 2rem)'
-          overflowX='hidden'
-          overflowY='hidden'
-          wrap={{ base: 'nowrap', md: 'nowrap' }}
-        >
-          {columns.map((column) => (
-            <Box
-              key={column.id}
-              flex={{ base: '1 1 100%', md: '1 1 24%' }}
-              mx={2}
-              my={{ base: 4, md: 0 }}
-            >
-              <TaskColumn
-                title={column.title}
-                tasks={column.tasks}
-                columnId={column.id} // Pass the column's id here
-              />
-            </Box>
-          ))}
-        </Flex>
-      </DndProvider>
-    </TaskBoardProvider>
+const TaskBoard = ({ columns, projectId, onUpdateColumns }) => {
+  const {taskColumns}= useTaskBoard();
+
+    return (
+    <DndProvider backend={HTML5Backend}>
+      <Flex
+        direction={{ base: 'column', md: 'row' }}
+        justifyContent='space-between'
+        w='100%'
+        h='calc(100vh - 2rem)'
+        overflowX='hidden'
+        overflowY='hidden'
+        wrap={{ base: 'nowrap', md: 'nowrap' }}
+      >
+        {taskColumns.map((column) => (
+          <Box
+            key={column.id}
+            flex={{ base: '1 1 100%', md: '1 1 24%' }}
+            mx={2}
+            my={{ base: 4, md: 0 }}
+          >
+            <TaskColumn
+              title={column.title}
+              tasks={column.tasks}
+              columnId={column.id}
+            />
+          </Box>
+        ))}
+      </Flex>
+    </DndProvider>
   );
 };
 
 export default TaskBoard;
-
