78a36db064d56e5a9a7a7a63e816f39d7deb623c
hudsonhrh <hudsonhrh7@gmail.com>
2023-04-01 13:57:53 -0500

    close to getting NFT check to work

--- my-app/src/contexts/Web3Context.js
+++ my-app/src/contexts/Web3Context.js
@@ -1,5 +1,5 @@
-import React, { createContext, useContext, useState } from 'react';
-import Web3 from 'web3';
+import { createContext, useContext, useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 
 const Web3Context = createContext();
 
@@ -7,12 +7,74 @@ export const useWeb3Context = () => {
   return useContext(Web3Context);
 };
 
+const CONTRACT_ADDRESS = '0x2B55E639d3191441651543D3A6b31e3FF0304b96';
+const INFURA_PROJECT_ID = process.env.REACT_APP_INFURA_PROJECT_ID;
+
 export const Web3Provider = ({ children }) => {
-  const [web3, setWeb3] = useState(new Web3(Web3.givenProvider || 'http://localhost:8545'));
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [signer, setSigner] = useState(null);
+
+  useEffect(() => {
+    const initProvider = async () => {
+      // Check if Web3 has been injected by MetaMask
+      if (typeof window.ethereum !== 'undefined') {
+        // Use MetaMask provider
+        const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+        await web3Provider.send('eth_requestAccounts', []);
+        setProvider(web3Provider);
+        const signer = web3Provider.getSigner();
+        setSigner(signer);
+        const accounts = await signer.getAddress();
+        setAccount(accounts);
+
+      } else {
+        // Fallback to Infura provider
+        const infuraProvider = new ethers.providers.InfuraProvider('mainnet', INFURA_PROJECT_ID);
+        setProvider(infuraProvider);
+        const privateKey = 'YOUR_PRIVATE_KEY_HERE';
+        const wallet = new ethers.Wallet(privateKey, infuraProvider);
+        const signer = wallet.connect(infuraProvider);
+        setSigner(signer);
+        const accounts = await signer.getAddress();
+        setAccount(accounts);
+      }
+    };
+
+    initProvider();
+  }, []);
+
+  const checkNFTOwnership = async () => {
+    console.log('provider:', provider);
+    console.log('account:', account);
+    console.log('signer:', signer);
+    if (provider && account && signer) {
+      try {
+        console.log('check1');
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, ['function balanceOf(address) returns (uint256)'], signer);
+        console.log('check2');
+        
+        const balance = await contract.balanceOf(account);
+
+        console.log('check3');
+        
+
+        console.log('NFT count:', balance.toNumber());
+
+        return balance.toNumber() > 0;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    }
+    return false;
+  };
 
   const value = {
-    web3,
-    setWeb3,
+    provider,
+    account,
+    signer,
+    checkNFTOwnership,
   };
 
   return <Web3Context.Provider value={value}>{children}</Web3Context.Provider>;
