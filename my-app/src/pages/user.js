c89b8be51dfa3c70138b3c65a4074fba32b3c0c0
hudsonhrh <hudsonhrh7@gmail.com>
2023-04-02 10:43:59 -0500

    deploy contract

--- my-app/src/pages/user.js
+++ my-app/src/pages/user.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import Web3 from "web3";
+import { ethers } from "ethers";
+import DirectDemocracyTokenArtifact from "../artifacts/contracts/DirectDemocracyToken.sol/DirectDemocracyToken.json";
 
 const User = () => {
   const [web3, setWeb3] = useState(null);
   const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
 
   useEffect(() => {
     const connectWallet = async () => {
@@ -28,14 +31,39 @@ const User = () => {
     connectWallet();
   }, []);
 
+  const deployContract = async () => {
+    if (!web3) {
+      console.log("Web3 not initialized");
+      return;
+    }
+
+    // Create an instance of the ethers.ContractFactory
+    const factory = new ethers.ContractFactory(
+      DirectDemocracyTokenArtifact.abi,
+      DirectDemocracyTokenArtifact.bytecode,
+      new ethers.providers.Web3Provider(window.ethereum).getSigner()
+    );
+
+    // Deploy the contract
+    const contractInstance = await factory.deploy();
+
+    // Wait for the contract deployment to be mined
+    await contractInstance.deployTransaction.wait();
+
+    // Update the state with the deployed contract
+    setContract(contractInstance);
+
+    console.log("Contract deployed at:", contractInstance.address);
+  };
+
   return (
     <div>
       {web3 && <p>Connected to blockchain network</p>}
       <p>Account: {account}</p>
+      <button onClick={deployContract}>Deploy DirectDemocracyToken Contract</button>
+      {contract && <p>Contract Address: {contract.address}</p>}
     </div>
   );
 };
 
 export default User;
-
-
