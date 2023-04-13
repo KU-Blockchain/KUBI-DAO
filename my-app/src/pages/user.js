24a8a3307f0283f9f58195d39ccf73e8dcd1bc98
hudsonhrh <hudsonhrh7@gmail.com>
2023-04-13 17:04:36 -0500

    Revert "added ask for address when mint exec nft"
    This reverts commit b4dd63ced9fda700289ce9663851a7b9206cb6b1.

--- my-app/src/pages/user.js
+++ my-app/src/pages/user.js
@@ -18,13 +18,6 @@ import {
   useToast,
   Flex,
   Heading,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  ModalFooter,
 } from "@chakra-ui/react";
 
 const PMContract= "0x5227970228DD9951e3e77a538a486221314Af06d"
@@ -50,9 +43,6 @@ const User = () => {
   const [execNftContract, setExecNftContract] = useState(null);
   const [execNftBalance, setExecNftBalance] = useState(0);
   const [showDeployMenu, setShowDeployMenu] = useState(false);
-  const [isMintModalOpen, setIsMintModalOpen] = useState(false);
-  const [mintAddress, setMintAddress] = useState("");
-
 
   const { userDetails, setUserDetails, account, setAccount, fetchUserDetails, addUserData, clearData } = useDataBaseContext();
 
@@ -199,25 +189,15 @@ const User = () => {
   const mintExecutiveNFT = async () => {
     if (!execNftContract) return;
     try {
-      await execNftContract.methods.mint(mintAddress).send({ from: account });
+      await execNftContract.methods.mint(account).send({ from: account });
       toast({ title: "Success", description: "Successfully minted Executive NFT", status: "success", duration: 5000, isClosable: true });
     } catch (error) {
       console.error(error);
       toast({ title: "Error", description: "Error minting Executive NFT", status: "error", duration: 5000, isClosable: true });
     }
-    closeMintModal();
-  };
-  
-
-  const openMintModal = () => {
-    setIsMintModalOpen(true);
   };
 
-  const closeMintModal = () => {
-    setIsMintModalOpen(false);
-    setMintAddress("");
-  };
-  
+
 
   const handleJoin = async () => {
     if (!email.endsWith("@ku.edu")) {
@@ -400,7 +380,7 @@ const User = () => {
               Deploy KUBIX token Contract
             </Button>
             {deployedKUBIXContract && <Text mt={4}>Contract address: {deployedKUBIXContract.options.address}</Text>}
-            <Button colorScheme="purple" mt={4} onClick={openMintModal}>
+            <Button colorScheme="purple" mt={4} onClick={mintExecutiveNFT}>
               Mint Executive NFT
             </Button>
             <Button colorScheme="purple" mt={4} onClick={clearData}>
@@ -409,31 +389,6 @@ const User = () => {
           </>
         )}
       </Flex>
-        <Modal isOpen={isMintModalOpen} onClose={closeMintModal}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Mint Executive NFT</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <FormControl id="mintAddress">
-                <FormLabel>Account Address</FormLabel>
-                <Input
-                  type="text"
-                  placeholder="Enter account address"
-                  value={mintAddress}
-                  onChange={(event) => setMintAddress(event.target.value)}
-                />
-              </FormControl>
-            </ModalBody>
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={mintExecutiveNFT}>
-                Mint
-              </Button>
-              <Button onClick={closeMintModal}>Cancel</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-
     </Flex>
   );
 };
