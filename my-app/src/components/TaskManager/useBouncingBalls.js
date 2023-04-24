8abc8544d1c5c9a1fbe5edcba88dd2a2d9f2cdb5
hudsonhrh <hudsonhrh7@gmail.com>
2023-04-24 13:49:46 -0500

    made balls bounce better

--- my-app/src/components/TaskManager/useBouncingBalls.js
+++ my-app/src/components/TaskManager/useBouncingBalls.js
@@ -4,8 +4,8 @@ import { useState, useEffect, useRef } from 'react';
 
 const useBouncingBalls = (containerRef) => {
   const redBallPos = useRef({ x: 200, y: 200 });
-  const blueBallPos = useRef({ x: 1000, y: 300 });
-  const blackBallPos = useRef({ x: 700, y: 800 });
+  const blueBallPos = useRef({ x: 600, y: 300 });
+  const blackBallPos = useRef({ x: 700, y: 100 });
   const [_, setRender] = useState({});
 
   useEffect(() => {
@@ -17,7 +17,7 @@ const useBouncingBalls = (containerRef) => {
 
     let redBallVelocity = { x: 0.25, y: .5 };
     let blueBallVelocity = { x: -1, y: .5 };
-    let blackBallVelocity = { x: -0.5, y: 1 };
+    let blackBallVelocity = { x: -0.5, y: -1 };
     let animationFrameId;
 
     const updatePositions = () => {
@@ -36,24 +36,24 @@ const useBouncingBalls = (containerRef) => {
         y: blackBallPos.current.y + blackBallVelocity.y,
       };
 
-      if (newPosRedBall.x < 0 || newPosRedBall.x + 100 > containerRect.width) {
+      if (newPosRedBall.x < 0 || newPosRedBall.x + 100 > containerRect.width-300) {
         redBallVelocity.x *= -1;
       }
-      if (newPosRedBall.y < 0 || newPosRedBall.y + 100 > containerRect.height) {
+      if (newPosRedBall.y < 0 || newPosRedBall.y + 100 > containerRect.height-175) {
         redBallVelocity.y *= -1;
       }
 
-      if (newPosBlueBall.x < 0 || newPosBlueBall.x + 100 > containerRect.width) {
+      if (newPosBlueBall.x < 0 || newPosBlueBall.x + 100 > containerRect.width-250) {
         blueBallVelocity.x *= -1;
       }
-      if (newPosBlueBall.y < 0 || newPosBlueBall.y + 100 > containerRect.height) {
+      if (newPosBlueBall.y < 0 || newPosBlueBall.y + 100 > containerRect.height-250) {
         blueBallVelocity.y *= -1;
       }
 
-      if (newPosBlackBall.x < 0 || newPosBlackBall.x + 100 > containerRect.width) {
+      if (newPosBlackBall.x < 0 || newPosBlackBall.x + 100 > containerRect.width-300) {
         blackBallVelocity.x *= -1;
       }
-      if (newPosBlackBall.y < 0 || newPosBlackBall.y + 100 > containerRect.height) {
+      if (newPosBlackBall.y < 0 || newPosBlackBall.y + 100 > containerRect.height-100) {
         blackBallVelocity.y *= -1;
       }
 
