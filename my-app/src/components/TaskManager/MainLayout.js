012cdefa9e81b17fa5dc37132bcdf4fe5f238e47
Vegito2367 <tejaaditya.g@gmail.com>
2023-04-25 22:37:24 -0500

    Made new Project description component

--- my-app/src/components/TaskManager/MainLayout.js
+++ my-app/src/components/TaskManager/MainLayout.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Flex, Box, Heading,} from '@chakra-ui/react';
+import { Flex, Box, Heading, Button,} from '@chakra-ui/react';
 import ProjectSidebar from './ProjectSidebar';
 import TaskBoard from './TaskBoard';
 import { TaskBoardProvider } from '../../contexts/TaskBoardContext';
 import { useDataBaseContext, DataBaseProvider } from '../../contexts/DataBaseContext';
 import { useWeb3Context, Web3Provider } from '../../contexts/Web3Context';
 import { Alert } from '@chakra-ui/alert';
+import ProjectDescription from './ProjectDescription';
 
 
 const MainLayout = () => {
@@ -20,6 +21,8 @@ const MainLayout = () => {
 
   const {account}= useWeb3Context()
   const [projectName,setProjectName]=useState('No Project Selected');
+
+  const [activeTab,setActiveTab]=useState(true)
   
   React.useEffect(() => {
     console.log("Mounted")
@@ -50,33 +53,50 @@ const MainLayout = () => {
     <Flex direction="column" w="100%" minH="85vh">
       <Box bg={"cornflowerblue"} p={4} boxShadow="md">
         <Heading mr={"300px"}color={'white'} size="md">{projectName}</Heading>
+        <Button 
+        textColor={(activeTab ? 'white' : 'black')}
+        _hover={{bg:"red.400"}}
+        bg={activeTab ? 'red' : 'ghostwhite'}
+        onClick={()=>setActiveTab(true)}>Task Board</Button>
+        <Button
+        textColor={(!activeTab ? 'white' : 'black')}
+        _hover={{bg:"red.400"}}
+        bg={(!activeTab) ? 'red' : 'ghostwhite'}
+        onClick={()=>setActiveTab(false)}>Project Description</Button>
       </Box>
-      <Flex w="100%" flex="1">
+      
+       <Flex w="100%" flex="1">
         <ProjectSidebar
           projects={projects}
           selectedProject={selectedProject}
           onSelectProject={handleSelectProject}
           onCreateProject={handleCreateProject}
         />
-        {selectedProject ? (
-          <TaskBoardProvider 
-            key={selectedProject.id}
-            projectId={selectedProject.id}
-            initialColumns={selectedProject.columns}
-            onColumnChange={(newColumns) => handleUpdateColumns(newColumns)}
-            onUpdateColumns={(newColumns) => handleUpdateColumns(newColumns)}
-            account={account}
-          >
-            <TaskBoard />
-          </TaskBoardProvider>
+        {activeTab ? (
+          selectedProject ? (
+            <TaskBoardProvider 
+              key={selectedProject.id}
+              projectId={selectedProject.id}
+              initialColumns={selectedProject.columns}
+              onColumnChange={(newColumns) => handleUpdateColumns(newColumns)}
+              onUpdateColumns={(newColumns) => handleUpdateColumns(newColumns)}
+              account={account}
+            >
+              <TaskBoard />
+            </TaskBoardProvider>
+          ) : (
+            <Flex flexGrow={1} justifyContent="center" alignItems="center">
+              <div>No projects selected, please create or select a project.</div>
+            </Flex>
+          )
         ) : (
-          <Flex flexGrow={1} justifyContent="center" alignItems="center">
-            <div>No projects selected, please create or select a project.</div>
-          </Flex>
+          <ProjectDescription />
         )}
+         
       </Flex>
     </Flex>
-  );
+    
+  ); 
 };
 
 export default MainLayout;
---
+++ my-app/src/components/TaskManager/ProjectDescription.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+const ProjectDescription = () => {
+  return(
+    <>
+      Hi test
+    </>
+  );
+};
+
+export default ProjectDescription
