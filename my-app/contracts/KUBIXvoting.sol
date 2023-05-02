5fec490c0b4aa720a0c14ed6a1983bf4e4adeac4
hudsonhrh <hudsonhrh7@gmail.com>
2023-05-02 14:30:08 -0500

    added all voting funtionality

--- my-app/contracts/KUBIXvoting.sol
+++ my-app/contracts/KUBIXvoting.sol
@@ -6,21 +6,27 @@ contract KubixVoting {
     IERC20 public kubixToken;
     address public dao;
 
+    struct PollOption {
+        string optionName;
+        uint256 votes;
+    }
+
     struct Proposal {
         string name;
         string description;
         string execution;
         uint256 totalVotes;
         mapping(address => uint256) votes;
-        bool isPoll;
         uint256 maxBalance;
         uint256 minBalance;
+        uint256 timeInMinutes;
+        uint256 creationTimestamp;
+        PollOption[] options;
     }
-
 
     Proposal[] public proposals;
 
-    event NewProposal(uint256 indexed proposalId, string name, bool isPoll);
+    event NewProposal(uint256 indexed proposalId, string name);
 
     constructor(address _kubixToken, address _dao) {
         kubixToken = IERC20(_kubixToken);
@@ -32,48 +38,61 @@ contract KubixVoting {
         _;
     }
 
-function createProposal(
-    string memory _name,
-    string memory _description,
-    string memory _execution,
-    bool _isPoll,
-    uint256 _maxBalance,
-    uint256 _minBalance
-) external onlyDAO {
-    require(_minBalance > 0, "Min balance must be greater than 0");
-    require(_maxBalance > _minBalance, "Max balance must be greater than min balance");
+    modifier whenNotExpired(uint256 _proposalId) {
+        Proposal storage proposal = proposals[_proposalId];
+        require(block.timestamp <= proposal.creationTimestamp + proposal.timeInMinutes * 1 minutes, "Voting time expired");
+        _;
+    }
 
-    uint256 newId = proposals.length;
+    function createProposal(
+        string memory _name,
+        string memory _description,
+        string memory _execution,
+        uint256 _maxBalance,
+        uint256 _minBalance,
+        uint256 _timeInMinutes,
+        string[] memory _options
+    ) external onlyDAO {
+        require(_minBalance > 0, "Min balance must be greater than 0");
+        require(_maxBalance > _minBalance, "Max balance must be greater than min balance");
 
-    proposals.push();
-    Proposal storage newProposal = proposals[newId];
-    newProposal.name = _name;
-    newProposal.description = _description;
-    newProposal.execution = _execution;
-    newProposal.totalVotes = 0;
-    newProposal.isPoll = _isPoll;
-    newProposal.maxBalance = _maxBalance;
-    newProposal.minBalance = _minBalance;
+        uint256 newId = proposals.length;
 
-    emit NewProposal(newId, _name, _isPoll);
-}
+        proposals.push();
+        Proposal storage newProposal = proposals[newId];
+        newProposal.name = _name;
+        newProposal.description = _description;
+        newProposal.execution = _execution;
+        newProposal.totalVotes = 0;
+        newProposal.maxBalance = _maxBalance;
+        newProposal.minBalance = _minBalance;
+        newProposal.timeInMinutes = _timeInMinutes;
+        newProposal.creationTimestamp = block.timestamp;
 
+        for (uint256 i = 0; i < _options.length; i++) {
+            newProposal.options.push(PollOption(_options[i], 0));
+        }
 
+        emit NewProposal(newId, _name);
+    }
 
-
-    function vote(uint256 _proposalId, address _voter) external {
+    function vote(uint256 _proposalId, address _voter, uint256 _optionIndex) external whenNotExpired(_proposalId) {
         Proposal storage proposal = proposals[_proposalId];
         require(proposal.votes[_voter] == 0, "Already voted");
 
         uint256 balance = kubixToken.balanceOf(_voter);
         require(balance > 0, "No KUBIX tokens");
+        
+        if (balance > proposal.maxBalance) {
+            balance = proposal.maxBalance;
+        }
 
         uint256 voteWeight = calculateVoteWeight(_proposalId, balance);
 
         proposal.totalVotes += voteWeight;
         proposal.votes[_voter] = voteWeight;
+        proposal.options[_optionIndex].votes += voteWeight;
     }
-
 
     function calculateVoteWeight(uint256 _proposalId, uint256 _balance) public view returns (uint256) {
         Proposal storage proposal = proposals[_proposalId];
@@ -83,4 +102,19 @@ function createProposal(
         return weight;
     }
 
+    function getWinner(uint256 _proposalId) external view returns (string memory) {
+        Proposal storage proposal = proposals[_proposalId];
+
+        uint256 highestVotes = 0;
+        uint256 winningOptionIndex;
+
+        for (uint256 i = 0; i < proposal.options.length; i++) {
+            if (proposal.options[i].votes > highestVotes) {
+                highestVotes = proposal.options[i].votes;
+                winningOptionIndex = i;
+            }
+        }
+
+        return proposal.options[winningOptionIndex].optionName;
+    }
 }
