8b694e8781cca51df0df74838b4e532f8ac09850
Vegito2367 <tejaaditya.g@gmail.com>
2023-09-10 21:54:31 -0500

    Laid proper foundation for nameboards

--- my-app/src/pages/practice.js
+++ my-app/src/pages/practice.js
@@ -1,5 +1,6 @@
 import React, { use, useState } from 'react';
-import '../styles/nameboards.module.css';
+import nameboard from '../styles/nameboards.module.css';
+import glass from '../styles/TaskColumn.module.css';
 import {
   Button,
   FormControl,
@@ -34,31 +35,31 @@ const AllExecs=[
     cardname: "Tej",
     description:"Hey guys, I am Tej Gumaste and I am a sophomore studying CS and Math. I love playing video games and sometimes making them too, and I also enjoy badminton and bad bitches.",
     picture:"Headshot_Tej.jpg",
-    id:1
+    id:nameboard.specialcolors
   },
   {
     cardname:"Hudson",
     description:"Hey guys, I am Tej Gumaste and I am a sophomore studying CS and Math. I love playing video games and sometimes making them too, and I also enjoy badminton and bad bitches.",
     picture:"Hudson",
-    id:"colors"
+    id:nameboard.specialblockchain
   },
   {
     cardname:"Emma",
     description:"Hey guys, I am Tej Gumaste and I am a sophomore studying CS and Math. I love playing video games and sometimes making them too, and I also enjoy badminton and bad bitches.",
     picture:"Emma",
-    id:"colors"
+    id:nameboard.specialEmma
   },
   {
     cardname: "Cameron",
     description:"Hey guys, I am Tej Gumaste and I am a sophomore studying CS and Math. I love playing video games and sometimes making them too, and I also enjoy badminton and bad bitches.",
     picture:"Headshot_Tej.jpg",
-    id:"colors"
+    id:nameboard.specialCameron
   },
   {
     cardname: "Jonathan",
     description:"Hey guys, I am Tej Gumaste and I am a sophomore studying CS and Math. I love playing video games and sometimes making them too, and I also enjoy badminton and bad bitches.",
     picture:"Headshot_Tej.jpg",
-    id:"colors"
+    id:nameboard.specialJonathan
   },
 ]
 
@@ -66,7 +67,7 @@ const AllExecs=[
 function cardClasses(idnum){
   console.log(idnum)
   switch(idnum){
-    case 1: return "specialcolors"
+    case "colors": return "specialcolors"
   }
 }
 
@@ -79,11 +80,23 @@ function practice()
     <Flex columnGap={6} margin={4} rowGap={6} flexWrap={"wrap"}>
       {AllExecs.map(exec =>(
       
-        <Card maxHeight={500} width={400} borderRadius={50} bgColor={"teal.200"} className="specialcolors">
-        <CardBody style={{display:"flex", flexDirection:"column"}}>
+        <div className={exec.id} style={{
+          maxHeight:"500px",
+          width:"400px",
+          borderRadius:"10px",
+          position:"relative"
+        }}>
+        <div className={glass} style={{
+          position:"absolute",
+          width:"100%",
+          height:"100%",
+          }}></div>
+        <div style={{display:"flex", flexDirection:"column"}}>
           <div style={{display:"flex"}}>
             <div style={{
               marginRight: "2rem",
+              paddingLeft:"1.5rem",
+              paddingTop:"1.5rem",
               height: "100%",
               display:"flex",
               flexDirection:"column",
@@ -92,24 +105,30 @@ function practice()
               <div style={{border:"solid",borderWidth:"3px", borderRadius:"50%"}}>
                 <Avatar name={exec.cardname} src={exec.picture}/>
               </div>
-              {exec.name}
+              {exec.cardname}
               
             </div>
             <div style={{
               display:"flex"
               
             }}>
-              <div>
+              <div style={{
+                paddingRight:"1rem"
+              }}>
                 {exec.description}
               </div>
               
             </div>
           </div>
           
+        </div>
+      </div>
+      ))}
+      <Card>
+        <CardBody>
+          hi its Tej
         </CardBody>
       </Card>
-      ))}
-      
     </Flex>
       
     </>
