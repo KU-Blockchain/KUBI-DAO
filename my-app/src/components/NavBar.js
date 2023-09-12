6e7b752d40877da2cb0fe9c1e0d31060f5f70c93
hudsonhrh <hudsonhrh7@gmail.com>
2023-09-11 20:17:16 -0500

    home button is now the KUBI logo

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -6,28 +6,27 @@ const Navbar = () => {
   return (
     <Box bg="black" p={4}>
       <Flex alignItems="center" h="100%" maxH="62px" maxW={"100%"} justifyContent="space-between">
-        <Box h="100%" w="12%"  mr={{ base: '4', md: '8' }}>
-          <Image
-            src="/images/KUBC-logo-RGB-1200.png"
-            alt="KUBC Logo"
-            w="auto"
-            maxW="65%"
-          />
+        <Box h="100%" w="12%" mr={{ base: '4', md: '8' }}>
+          <Link as={NextLink} href="/" passHref>
+              <Image
+                src="/images/KUBC-logo-RGB-1200.png"
+                alt="KUBC Logo"
+                w="auto"
+                maxW="65%"
+              />
+          </Link>
         </Box>
         <Flex justifyContent="space-between" flexGrow={1} ml={4} mr={4}>
-          <Link as={NextLink} href="/" color="white" fontWeight="extrabold" fontSize="xl" mx={"2%"}> 
-            Home
-          </Link>
           <Link as={NextLink} href="/tasks" color="white" fontWeight="extrabold" fontSize="xl" mx={"2%"}>
             Tasks
           </Link>
           <Link as={NextLink} href="/Leaderboard" color="white" fontWeight="extrabold" fontSize="xl" mx={"2%"}>
             Leaderboard
           </Link>
-          <Link as={NextLink} href="/voting" color="white" fontWeight="extrabold"  fontSize="xl" mx={"2%"}>
+          <Link as={NextLink} href="/voting" color="white" fontWeight="extrabold" fontSize="xl" mx={"2%"}>
             Voting
           </Link>
-          <Link as={NextLink} href="/user" color="white" fontWeight="extrabold"  fontSize="xl" mx={"2%"}>
+          <Link as={NextLink} href="/user" color="white" fontWeight="extrabold" fontSize="xl" mx={"2%"}>
             User
           </Link>
         </Flex>
